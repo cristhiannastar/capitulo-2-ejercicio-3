55ab4e9dc0234368c7f56e3c04a917c6fcec9285
Hp <Hp@DESKTOP-NVV8KB0>
2022-11-16 21:41:05 -0500

    Agregar archivos de proyecto.

---
+++ capitulo 2 ejercicio 3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ejercico23
+{
+
+    public class program
+    {
+        public static void Main ()
+        {
+            int numero;
+            int doblado;
+            int resultado;
+
+            Console.WriteLine("por favor ingrese un numero");
+            numero = Convert.ToInt32(Console.ReadLine());
+            doblado = numero * 2;
+            resultado = doblado * 25;
+            Console.WriteLine($"el resultado de la operacion es: {resultado}");
+
+
+        }
+
+    }
+}
